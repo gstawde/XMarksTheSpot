1d838281c33fd23f8b6006513c669bf1ac395250
fahmed0710 <fahmed0710@gmail.com>
2024-03-03 13:26:10 -0800

    Updated Signup page UI

--- dev/frontend/src/pages/SignupPage.js
+++ dev/frontend/src/pages/SignupPage.js
@@ -1,45 +1,51 @@
+import XMarksLogo from '../assets/XMarksLogo.png';
+
 const SignupPage = () => {
   return (
-    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-      <div class="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0"> 
+    <div class="bg-xmts-darkbrown flex items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 overflow-auto">
+      <div class="w-full md:mt-0 sm:max-w-md xl:p-0 overflow-auto">
+        <div class="flex justify-center h-24">         
+          <img class="max-h-full w-auto" src={XMarksLogo} alt="Image Logo" />
+        </div>
+        
+        <h1 class="text-xmts-yellow text-5xl text-center">Let's Get to Know You a Bit!</h1>
+        
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
-            Create an account
-          </h1>
-          
-          <form class="space-y-4 md:space-y-6" action="#">
+          <form class="space-y-4 md:space-y-6" action="x">
             <div>
-              <label for="fname" class="block mb-2 text-sm font-medium text-gray-900">First Name</label>
-              <input type="fname" name="fname" id="fname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5" placeholder="First Name" required=""></input>
+              <label for="fname" class="text-xmts-tan text-lgfont-medium">First Name</label>
+              <input type="fname" name="fname" id="fname" placeholder="First Name" required
+                class="bg-xmts-tan rounded-lg w-full p-2"></input>
             </div>
+
             <div>
-              <label for="lname" class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-              <input type="lname" name="lname" id="lname" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5" placeholder="Last Name" required=""></input>
+              <label for="lname" class="text-xmts-tan text-lgfont-medium">Last Name</label>
+              <input type="lname" name="lname" id="lname" placeholder="Last Name" required
+                class="bg-xmts-tan rounded-lg w-full p-2"></input>
             </div>
+
             <div>
-              <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
-              <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg w-full p-2.5" placeholder="name@company.com" required=""></input>
+              <label for="email" class="text-xmts-tan text-lgfont-medium">Email</label>
+              <input type="email" name="email" id="email" placeholder="Email" required
+                class="bg-xmts-tan rounded-lg w-full p-2"></input>
             </div>
+
             <div>
-              <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Password</label>
-              <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" required=""></input>
+              <label for="password" class="text-xmts-tan text-lgfont-medium">Password</label>
+              <input type="password" name="password" id="password" placeholder="••••••••" required
+                class="bg-xmts-tan rounded-lg w-full p-2"></input>
             </div>
+
             <div>
-              <label for="confirm-password" class="block mb-2 text-sm font-medium text-gray-900">Confirm password</label>
-              <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2.5" required=""></input>
+              <label for="confirm-password" class="text-xmts-tan text-lgfont-medium">Confirm password</label>
+              <input type="confirm-password" name="confirm-password" id="confirm-password" placeholder="••••••••" required
+                class="bg-xmts-tan rounded-lg w-full p-2"></input>
             </div>
-            <div class="flex items-start">
-              <div class="flex items-center h-5">
-                <input id="terms" aria-describedby="terms" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50" required=""></input>
-              </div>
-              <div class="ml-3 text-sm">
-                <label for="terms" class="font-light text-gray-500">I accept the <a class="font-medium text-primary-600 hover:underline" href="#">Terms and Conditions</a></label>
-               </div>
+            
+            <div class="flex items-center justify-center">
+               {/*When user signs up, they should be redirected to user dashboard*/}
+              <button type="submit" class="bg-xmts-yellow w-1/3 font-medium rounded-lg px-5 py-2.5 text-center">Sign Up</button>
             </div>
-            <button type="submit" class="w-full text-white bg-slate-800 hover:bg-primary-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create an account</button>
-            <p class="text-sm font-light text-gray-500">
-              Already have an account? <a href="/login" class="font-medium text-primary-600 hover:underline">Login here</a>
-            </p>
           </form>
         </div>
       </div>
--- dev/frontend/tailwind.config.js
+++ dev/frontend/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        dynapuff: ["DynaPuff", "sans-serif"],
+      },
+      colors: {
+        'xmts-black': '#000000',
+        'xmts-darkbrown': '#211B11',
+        'xmts-yellow': '#F5B900',
+        'xmts-red': '#F50000',
+        'xmts-lightbrown': '#7D633F',
+        'xmts-tan': '#D7BC95'
+      }
+    },
   },
   plugins: [],
 }
