c241b892b8bedfd1b8bf072d32465f8f75634d5b
fahmed0710 <fahmed0710@gmail.com>
2024-04-21 22:18:23 -0700

    Created API to generate fill-in-the-blank question

--- dev/backend/apis/question_apis.py
+++ dev/backend/apis/question_apis.py
@@ -23,14 +23,19 @@ def get_countries():
   except Exception as e:
     return jsonify({'success': False, 'message': str(e)})
 
+countries = get_countries()
+
 @app.route('/question/get/random', methods=['GET'])
-def get_random_question():
+def get_random_question(question_types=None):
   try:
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor(dictionary=True)
 
     cursor.execute("SELECT * FROM Questions")
     questions = cursor.fetchall()
+
+    if question_types is not None:
+      questions = [q for q in questions if q["question_type"] in question_types]
 
     question = random.sample(questions, 1)[0]
 
@@ -43,19 +48,15 @@ def get_mc_question():
   try:
     random_question = get_random_question()
     question = random_question["question"] 
-    question_type = random_question["question_type"]
     answer_field = random_question["answer_field"]
     answer = random_question["answer"]
     choice_type = get_choice_type(random_question)
-    
-    countries = get_countries()
     
     if answer is not None: 
       correct_countries = [country for country in countries if country[answer_field] == answer]
       correct_option = random.sample(correct_countries, 1)[0]
     else:
       correct_option = random.sample(countries,1)[0]
-
 
     incorrect_countries = [country for country in countries if country[answer_field] != correct_option[answer_field]]
     if len(incorrect_countries) >= 3:
@@ -68,3 +69,25 @@ def get_mc_question():
     return jsonify({'success': True, 'message': 'Multiple choice question generated successfully.', 'result': mc_question})
   except Exception as e:
     return jsonify({'success': False, 'message': str(e)})
+
+@app.route('/question/get/fib', methods=['GET'])
+def get_fib_question():
+  try:
+    random_question = get_random_question(["Identification", "Fact Retrieval"])
+    question = random_question["question"] 
+    answer_field = random_question["answer_field"]
+    answer = random_question["answer"]
+    choice_type = get_choice_type(random_question)
+
+    if answer is not None: 
+      correct_countries = [country for country in countries if country[answer_field] == answer]
+      correct_option = random.sample(correct_countries, 1)[0]
+    else:
+      correct_option = random.sample(countries,1)[0]    
+    
+    fib_question = {"question": question, "choice_type": choice_type, "correct_option": correct_option}
+
+    return jsonify({'success': True, 'message': 'Fill-in-the-blank question generated successfully', 'result': fib_question})
+  except Exception as e:
+    return jsonify({'success': False, 'message': str(e)})
+  
