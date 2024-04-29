a9bf1d7bc532ce5291d4791b9b7518bceaf5178d
fahmed0710 <fahmed0710@gmail.com>
2024-04-28 17:18:26 -0700

    Fixed bug in password validity check in Signup page

--- dev/frontend/src/pages/SignupPage.js
+++ dev/frontend/src/pages/SignupPage.js
@@ -37,16 +37,17 @@ const SignupPage = () => {
 
   const validatePassword = (e) => {
     const pw = e.target.value;
+    console.log(pw);
     setPassword(pw);
 
-    const passwordRegex =
-      /^.{8,}$/;
+    const passwordRegex = /^.{8,}$/;
 
-    if (!passwordRegex.test(password)) {
+    console.log(passwordRegex.test(pw));
+    if (!passwordRegex.test(pw)) {
       setPasswordError(true);
     } else {
       setPasswordError(false);
-      //confirmPassword();
+      confirmPassword();
     }
   };
 
@@ -55,7 +56,7 @@ const SignupPage = () => {
     setConfirmPW(pw);
     console.log(password, confirmPW);
 
-    if (confirmPW !== password) {
+    if (pw !== password) {
       setConfirmPasswordError(true);
     } else {
       setConfirmPasswordError(false);
@@ -70,7 +71,7 @@ const SignupPage = () => {
       setError(true);
       setErrorMessage(["Check your information again!", "Make sure all the required information is filled out."]);
       return;
-    } else if (password.length <= 8) {
+    } else if (password.length + 1 <= 8) {
       setError(true);
       setErrorMessage(["Check your information again!", "Password must be at least 8 characters."]);
       return;
@@ -111,19 +112,19 @@ const SignupPage = () => {
   };
 
   return (
-    <div class="bg-xmts-darkbrown flex items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 overflow-auto">
-      <div class="w-full md:mt-0 sm:max-w-md xl:p-0 overflow-auto">
+    <div className="bg-xmts-darkbrown flex items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 overflow-auto">
+      <div className="w-full md:mt-0 sm:max-w-md xl:p-0 overflow-auto">
         
-        <div class="flex justify-center h-24">
-          <img class="max-h-full w-auto" src={XMarksLogo} alt="Image Logo" />
+        <div className="flex justify-center h-24">
+          <img className="max-h-full w-auto" src={XMarksLogo} alt="Image Logo" />
         </div>
 
-        <h1 class="text-xmts-yellow text-5xl text-center">
+        <h1 className="text-xmts-yellow text-5xl text-center">
           Let's Get to Know You a Bit!
         </h1>
         
-        <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-          <form class="flex flex-col gap-6">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <form className="flex flex-col gap-6">
             <div>
               <Input
                 type="fname" label="First Name" required
@@ -152,57 +153,13 @@ const SignupPage = () => {
                 onChange={(e) => { setEmail(e.target.value); }}   
               />
             </div>
-            <div class="relative">
+            <div className="relative">
               <Input
                 type={showPassword ? "text" : "password"} label="Password" required
                 className="w-64 bg-xmts-tan" color="white"
                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}"
                 onChange={ validatePassword }   
               />
-
-              <label class="swap swap-rotate absolute inset-y-0 right-0 flex items-center mr-3">
-                <input
-                    type="checkbox" className="hidden"
-                    onClick={togglePasswordVisibility}  
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px" height="20px" viewBox="0 0 16 16"
-                  class="fill-current w-5 h-5 swap-off"
-                >
-                  <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"></path>
-                  <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"></path>
-                  <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"></path>
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px" height="20px" viewBox="0 0 24 24"
-                  fill="none"
-                  class="w-5 h-5 fill-none swap-on"
-                >
-                  <path
-                    d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <circle
-                    cx="12" cy="12" r="3"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </label>
               
               {passwordError && (
                 <p class="text-xmts-red text-sm py-2">
@@ -210,69 +167,25 @@ const SignupPage = () => {
                 </p>
               )}
             </div>
-            <div class="relative">
+            <div className="relative">
               <Input
                 type={showConfirmPW ? "text" : "password"} label="Confirm Password" required
                 className="w-64 bg-xmts-tan" color="white"
                 pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}"
                 onChange={ confirmPassword }   
               />
-              <label class="swap swap-rotate absolute inset-y-0 right-0 flex items-center mr-3">
-                <input
-                  type="checkbox"
-                  onClick={toggleConfirmPasswordVisibility}
-                  className="hidden"
-                />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px" height="20px" viewBox="0 0 16 16"
-                  class="fill-current w-5 h-5 swap-off"
-                >
-                  <path d="M13.359 11.238C15.06 9.72 16 8 16 8s-3-5.5-8-5.5a7.028 7.028 0 0 0-2.79.588l.77.771A5.944 5.944 0 0 1 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.134 13.134 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755-.165.165-.337.328-.517.486l.708.709z"></path>
-                  <path d="M11.297 9.176a3.5 3.5 0 0 0-4.474-4.474l.823.823a2.5 2.5 0 0 1 2.829 2.829l.822.822zm-2.943 1.299.822.822a3.5 3.5 0 0 1-4.474-4.474l.823.823a2.5 2.5 0 0 0 2.829 2.829z"></path>
-                  <path d="M3.35 5.47c-.18.16-.353.322-.518.487A13.134 13.134 0 0 0 1.172 8l.195.288c.335.48.83 1.12 1.465 1.755C4.121 11.332 5.881 12.5 8 12.5c.716 0 1.39-.133 2.02-.36l.77.772A7.029 7.029 0 0 1 8 13.5C3 13.5 0 8 0 8s.939-1.721 2.641-3.238l.708.709zm10.296 8.884-12-12 .708-.708 12 12-.708.708z"></path>
-                </svg>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20px" height="20px" viewBox="0 0 24 24"
-                  fill="none"
-                  class="w-5 h-5 fill-none swap-on"
-                >
-                  <path
-                    d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <circle
-                    cx="12" cy="12" r="3"
-                    stroke="#000000"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </label>
               
               {confirmPasswordError && (
-                <p class="text-xmts-red text-sm py-2">Passwords must match.</p>
+                <p className="text-xmts-red text-sm py-2">Passwords must match.</p>
               )}
             </div>
 
             {error && (
               <div
-                class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
+                className="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50"
                 role="alert"
               >
-                <span class="font-medium">{errorMessage[0]}</span>{" "}
+                <span className="font-medium">{errorMessage[0]}</span>{" "}
                 {errorMessage[1]}
               </div>
             )}
