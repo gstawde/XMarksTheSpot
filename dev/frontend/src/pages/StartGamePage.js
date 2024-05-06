d209d21f2a519102adbfb89b140e726aefe961a3
fahmed0710 <fahmed0710@gmail.com>
2024-05-05 23:42:09 -0700

    Minor fixes

--- dev/frontend/src/pages/StartGamePage.js
+++ dev/frontend/src/pages/StartGamePage.js
@@ -200,7 +200,7 @@ const StartGame = () => {
    
         <div className="game-users">
           {users.map((user) => (
-            <div key={user.id} className="user-positioning">{user.username}</div>
+            <div key={user.id} className="user-positioning text-2xl">{user.username}</div>
           ))}
         </div>
       </body>
